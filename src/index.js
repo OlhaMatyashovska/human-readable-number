71b500a0115c65cb5a7e14651ff626cc463a43d3
Olga Matyashovska <matyashowskamaria@gmail.com>
2021-03-03 20:28:19 +0200

    feat: learned how to make numbers readaable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function toReadable (number) {
-  
+  return convert(number).trim();
+}
+let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+function convert(number) {
+  if(number == 0) {
+      return "zero";
+  } else {
+      return convertToHundreds(number);
+  }
+}
+
+function convertToHundreds(number) {
+  if(number >= 100) {
+      return ones[Math.floor(number/100)] + " hundred " + convertToTens(number % 100);
+  } else{
+      return convertToTens(number);
+  }
+}
+
+function convertToTens(number) {
+  if(number >= 20) {
+      return tens[Math.floor(number / 10)] + " " + ones[number % 10];
+  } else if(number >=10 && number <=19){
+     return teens[number-10];
+  }
+  else {
+      return ones[number];
+  }
 }
